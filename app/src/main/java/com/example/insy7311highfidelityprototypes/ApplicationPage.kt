294128112e92f530c348cc841214a43a5dc5d511
ST10401917 <st10401917@vcconnect.edu.za>
2026-05-29 11:53:43 +0200

    Fix errors

--- app/src/main/java/com/example/insy7311highfidelityprototypes/ApplicationPage.kt
+++ app/src/main/java/com/example/insy7311highfidelityprototypes/ApplicationPage.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.insy7311highfidelityprototypes.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ApplicationPage : AppCompatActivity() {
